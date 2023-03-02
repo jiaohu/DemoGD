0317b02d70fea2de08eaa8da948ca8dd9e0402d3
likai <likai@hyperchain.cn>
2023-03-02 10:16:03 +0800

    init

---
+++ Main.gd
@@ -0,0 +1,62 @@
+extends Node
+
+export(PackedScene) var mob_scene
+
+var score
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	
+func new_game():
+	get_tree().call_group("mobs", "queue_free")
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")	
+	
+
+
+func _on_MobTimer_timeout():
+	var mob = mob_scene.instance()
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	mob.position = mob_spawn_location.position
+	
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(rand_range(160.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
